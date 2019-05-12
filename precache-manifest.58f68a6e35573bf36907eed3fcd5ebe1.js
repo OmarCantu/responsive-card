89176d8ea4f216664a1ce9f1b8ca2231f3824cd2
Omar Cantu <OmarCantu@users.noreply.github.com>
2019-05-12 20:11:52 +1000

    Updates

---
+++ precache-manifest.58f68a6e35573bf36907eed3fcd5ebe1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ab94cc128e103dab82d5ad602263a8e",
+    "url": "/responsive-card/index.html"
+  },
+  {
+    "revision": "544825b6f916d24fa566",
+    "url": "/responsive-card/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "de2bf3f4651d8ce777cc",
+    "url": "/responsive-card/static/js/2.26c08ad2.chunk.js"
+  },
+  {
+    "revision": "544825b6f916d24fa566",
+    "url": "/responsive-card/static/js/main.ef78b395.chunk.js"
+  },
+  {
+    "revision": "6166b72b15ddffb15079",
+    "url": "/responsive-card/static/js/runtime~main.d88d4865.js"
+  },
+  {
+    "revision": "2a3f011a74b71cac77f0134976aad150",
+    "url": "/responsive-card/static/media/logo.2a3f011a.png"
+  },
+  {
+    "revision": "25b4e634dae07d5d77ebe67a3d0743b2",
+    "url": "/responsive-card/static/media/tile.25b4e634.jpg"
+  }
+]);
